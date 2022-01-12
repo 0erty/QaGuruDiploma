12a0af390b6ff81b56c88aeb77de38e74ce98625
0erty <derty9726@gmail.com>
2022-01-12 20:17:59 +0300

    Deleted unnecessary files

--- src/test/java/com/quadcode/config/demowebshop/App.java
+++
@@ -1,7 +0,0 @@
-package com.quadcode.config.demowebshop;
-
-import org.aeonbits.owner.ConfigFactory;
-
-public class App {
-    public static AppConfig config = ConfigFactory.create(AppConfig.class, System.getProperties());
-}
